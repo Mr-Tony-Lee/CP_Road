92fdfeeb8e2208c68a9364fd1537f34d5233c043
Mr-Tony-Lee <lchengyan49@gmail.com>
2025-06-01 21:26:50 +0800

    自動提交: 2025-06-01,21:26:50

--- Uva_record/UVA_1_star_question/10055.cpp
+++ Uva_record/UVA_1_star_question/10055.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main(){
     long long int number1 , number2 ;
-    cin >> number1 >> number2;
-    cout << abs(number1-number2) << endl;
+    while(cin >> number1 >> number2){
+        cout << abs(number1-number2) << endl;
+    }
 }
 //5ms, 332KB
+
+// 解題報告: 根據題意，輸出兩數相減的絕對值
